d2104d83d23b2876341de4bc36601a4ae48b3ec3
Łukasz Pędzimąż <pedzimaz.lukasz@gmail.com>
2023-05-16 12:31:07 +0200

    currentVersion

--- ProjectManagement/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ ProjectManagement/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -30,7 +30,7 @@ namespace ProjectManagement.Areas.Identity.Pages.Account
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
-                return LocalRedirect(returnUrl);
+                return RedirectToPage("./Login");
             }
             else
             {
